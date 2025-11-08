713c9114c5d367762ed5a07a320d700f498a16a3
Gesii69 <orgesselmani1@gmail.com>
2025-11-08 16:41:48 +0100

    projekti

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -17,3 +17,17 @@
             echo trim($output, $separator);
             ?>
         </span>
+        <span class="comment"><a href="#comments"><i class="fa fa-comment"></i> <?php comments_number('0','1','%'); ?></a></span>
+        </div>
+    </header>
+    
+            <?php the_content(); ?>
+
+           
+            <?php
+                comments_template();
+            ?>
+                 
+        </div>
+
+    
